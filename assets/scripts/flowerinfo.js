14303fd50c0d37810cb49b8e0aa8feb847951640
EllaPellaa <112462853+EllaPellaa@users.noreply.github.com>
2024-10-21 15:47:49 +0300

    table testing

--- assets/scripts/flowerinfo.js
+++ assets/scripts/flowerinfo.js
@@ -9,4 +9,19 @@ flowers.push(new Flower("hydrangea", "hydrangea.jpg", ["may", "june", "july"]));
 flowers.push(new Flower("lily of the valley", "lilyofthevalley.jpg", ["may"]));
 flowers.push(new Flower("rose", "rose.jpg", ["may", "june", "july", "august", "september", "october"]));
 
-console.log(flowers[0]);
+flowerinfotable = document.getElementById("flowerinfo");
+
+for (let i = 0; i < flowers.length; i++) {
+    flowerinfotable.innerHTML += `<tr>
+    <td>` + flowers[i].name + `</td>
+    <td>`;
+
+    for (let j = 0; j < flowers[i].blooms.length; j++) {
+        flowerinfotable.innerHTML += flowers[i].blooms[j]
+        if(j !== (flowers[i].blooms.length - 1)) {
+            flowerinfotable.innerHTML += ", ";
+        }
+    }
+
+    flowerinfotable.innerHTML += "</td></tr>";
+}
